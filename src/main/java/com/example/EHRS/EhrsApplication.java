b94b9362890b58209377828c4b12716d637079e1
JAYARANIMANIMRAN <jayarani.m2023aiml@sece.ac.in>
2025-07-16 14:10:09 +0530

    Initial Commit

---
+++ src/main/java/com/example/EHRS/EhrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.EHRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EHRS/EhrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EHRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
